e964df5e306f0e1b0e3cbe520cbe3f8d3357ead9
Ana <analeonel93@gmail.com>
2024-08-12 12:05:57 -0300

    ShuffleAndFonts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
 
@@ -12,27 +13,44 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
-          currentQuestion.text,
-          style: const TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        const SizedBox(height: 30),
-        ...currentQuestion.answers.map((answer) {
-          return AnswerButton(
-            answerText: answer,
-            onTap: () {},
-          );
-        }),
-      ]),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                currentQuestion.text,
+                style: GoogleFonts.lato(
+                  color: const Color.fromARGB(255, 227, 200, 245),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+              ...currentQuestion.getShuffledAnswers().map((answer) {
+                return AnswerButton(
+                  answerText: answer,
+                  onTap: answerQuestion,
+                );
+              }),
+            ]),
+      ),
     );
   }
 }
